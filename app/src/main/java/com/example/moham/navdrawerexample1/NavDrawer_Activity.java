b03cce3d60b74e07168721c281426d65c1644b06
mohamedkhairy953 <mohamedsallam953@gmail.com>
2016-10-31 17:54:55 +0200

    SignUp Updates3

--- app/src/main/java/com/example/moham/navdrawerexample1/NavDrawer_Activity.java
+++ app/src/main/java/com/example/moham/navdrawerexample1/NavDrawer_Activity.java
@@ -35,7 +35,7 @@ public class NavDrawer_Activity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -87,6 +87,7 @@ public class NavDrawer_Activity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
             firebaseAuth.signOut();
             startActivity(new Intent(NavDrawer_Activity.this, LoginActivity.class));
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
